c7d1902f8e4471c8e5caca08a30ebf2aada6134a
Sinan Uyğun <sinan_uygun@windowslive.com>
2025-12-24 21:43:59 +0300

    ADD DataContainer

---
+++ core/include/context/datacontainer.h
@@ -0,0 +1,42 @@
+
+#ifndef DATA_CONTAINER_H
+#define DATA_CONTAINER_H
+
+#include <string>
+#include <vector>
+
+namespace context
+{
+    enum class Dimension
+    {
+        D2,
+        D2_5,
+        D3,
+        NONE
+    };
+    enum class DimensionType
+    {
+        PIXEL_ART,
+        STYLIZED,
+        REALISTIC,
+        VOXEL,
+        DEFAULT
+    };
+    enum class MemoryStrategy
+    {
+        ARENA,
+        DYNAMIC,
+        NONE
+    };
+
+    struct DataContainer
+    {
+        std::string project_name = "NewTurquazProject";
+        Dimension dimension = Dimension::NONE;
+        DimensionType dimension_type = DimensionType::DEFAULT;
+        MemoryStrategy memoryMode = MemoryStrategy::ARENA;
+        size_t memoryBudgetMB = 256;
+    };
+
+}
+#endif
