028fb97241850cbb6c99eb4789093f96ccf38cb7
Sinan Uyğun <sinan_uygun@windowslive.com>
2026-01-06 00:03:45 +0300

    FIX SettingParser

--- core/src/lexer/SettingParser.cpp
+++ core/src/lexer/SettingParser.cpp
@@ -1,9 +1,11 @@
 #include "../../include/lexer/SettingParser.h"
 #include "../../include/utils/ErrorHandler.h"
+#include "../../include/context/ProjectContext.h"
+#include <iostream>
 
 namespace TQS
 {
-    SettingsParser::SettingsParser(const std::string &source) : m_source(source) {}
+    SettingsParser::SettingsParser(const std::string &source) : m_source(source), m_cursor(0) {}
 
     void SettingsParser::skipWhitespace()
     {
@@ -17,70 +19,87 @@ namespace TQS
     {
         skipWhitespace();
         std::string word = "";
-        while (m_cursor < m_source.length() && std::isalnum(m_source[m_cursor]))
+
+        while (m_cursor < m_source.length() && (std::isalnum(m_source[m_cursor]) || m_source[m_cursor] == '_'))
         {
-            word += m_source[m_cursor];
+            word += m_source[m_cursor++];
         }
         return word;
     }
 
     bool SettingsParser::parse()
     {
+        std::cout << "[Debug] Parsing basladi..." << std::endl;
+
+        std::string header = getNextWord();
+        std::cout << "[Debug] Baslik: " << header << std::endl;
+
+        if (header != "ProjectSettings")
+            return false;
+
         skipWhitespace();
-        if (getNextWord() != "ProjectSettings")
-            return false;
-        if (m_source[m_cursor++] != ':')
-            return false;
+        if (m_cursor < m_source.length() && m_source[m_cursor] == ':')
+        {
+            m_cursor++;
+            std::cout << "[Debug] ':' atlandi." << std::endl;
+        }
 
         ProjectSetting settings;
 
+        // 3. Blok içindeki anahtarları oku
         while (m_cursor < m_source.length())
         {
+            skipWhitespace();
             std::string key = getNextWord();
+
             if (key == "end")
+            {
+                std::cout << "[Debug] 'end' bulundu." << std::endl;
                 break;
-            if (m_source[m_cursor++] != ':')
-                break;
+            }
+
+            if (key.empty())
+            {
+                m_cursor++;
+                continue;
+            }
 
             skipWhitespace();
+            if (m_cursor < m_source.length() && m_source[m_cursor] == ':')
+                m_cursor++;
 
-            if (key == "dimension")
+            std::string value = getNextWord();
+            std::cout << "[Debug] Anahtar: " << key << " | Deger: " << value << std::endl;
+
+            if (key == "name")
             {
-                std::string val = getNextWord();
-
-                if (val == "2D")
+                settings.project_name = value;
+            }
+            else if (key == "dimension")
+            {
+                if (value == "2D")
                     settings.dimension = Dimension::D2;
-                else if (val == "2.5D")
-                    settings.dimension = Dimension::D2_5;
-                else if (val == "3D")
+                else if (value == "3D")
                     settings.dimension = Dimension::D3;
-                else
-                    settings.dimension = Dimension::NONE;
             }
             else if (key == "type")
             {
-                std::string val = getNextWord();
-
-                if (val == "PixelArt")
+                if (value == "PixelArt")
                     settings.dimension_type = DimensionType::PIXEL_ART;
-                else if (val == "Realistic")
+                else if (value == "Realistic")
                     settings.dimension_type = DimensionType::REALISTIC;
-                else if (val == "Stylized")
-                    settings.dimension_type = DimensionType::STYLIZED;
-                else if (val == "Voxel")
-                    settings.dimension_type = DimensionType::VOXEL;
-                else
-                    settings.dimension_type = DimensionType::DEFAULT;
             }
-            else if (key == "memorybudget")
+            else if (key == "memory_budget")
             {
-                std::string val = getNextWord();
-                settings.memoryBudgetMB = std::stoi(val);
+                settings.memoryBudgetMB = std::stoi(value);
             }
 
-            if (m_source[m_cursor] == ';')
+            // ';' karakterini atla
+            skipWhitespace();
+            if (m_cursor < m_source.length() && m_source[m_cursor] == ';')
                 m_cursor++;
         }
+
         ProjectContext::getInstance().configure(settings);
         return true;
     }
---
+++ docs/NOTE.md
@@ -0,0 +1,5 @@
+OUTPUD:
+g++ -std=c++17 main.cpp test/ActOne.cpp core/src/context/*.cpp core/src/lexer/*.cpp core/src/memory/*.cpp core/src/utils/*.cpp -I core/include -o archive/actone_test_2.exe  
+
+RUN EXE:
+.\archive\actone_test_1.exe  
